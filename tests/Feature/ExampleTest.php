1fe34af4d6abdcf97af58eba4363756a814d8530
sacibusa-lab <sacibusa@gmail.com>
2026-06-13 10:37:36 +0100

    result report error updated

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect(route('login'));
+        $response->assertRedirect(route('public.results.form'));
     }
 }
